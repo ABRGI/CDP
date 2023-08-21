8f707bc4c0a250f8bca039726a72cd386eae17bd
Mika Hirvikorpi <mikahirvikorpi@kiva.fi>
2023-08-21 10:49:59 +0300

    Add dataset id env

--- functions/src/env.ts
+++ functions/src/env.ts
@@ -1,10 +1,10 @@
 export const googleProjectId = process.env.GCP_PROJECT_ID || "undefined"
-export const reservationTopicId = process.env.GCP_RESERVATION_TOPIC_ID || "undefined"
+export const datasetId = process.env.GCP_DATASET_ID || "undefined"
 
 if (googleProjectId === "undefined") {
   throw Error("GCP_PROJECT_ID environment variable not defined")
 }
 
-if (reservationTopicId === "undefined") {
-  throw Error("GCP_RESERVATION_TOPIC_ID environment variable not defined")
+if (datasetId === "undefined") {
+  throw Error("GCP_DATASET_ID environment variable not defined")
 }

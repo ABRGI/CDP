b22420e676760d90ce7dba7aa0a75c27d12695c3
Mika Hirvikorpi <mikahirvikorpi@kiva.fi>
2023-08-21 14:08:25 +0300

    Add scheduled triggers for fetch and merge

---
+++ infra/modules/cdp-bigquery/scheduled.tf
@@ -0,0 +1,48 @@
+resource "google_pubsub_topic" "function_trigger_fetch_pubsub" {
+  name    = "trigger-fetch-pubsub"
+  project = var.project_id
+
+  message_storage_policy {
+    allowed_persistence_regions = [
+      "europe-west1",
+    ]
+  }
+}
+
+resource "google_cloud_scheduler_job" "fetch_reservations_scheduler" {
+  project     = var.project_id
+  region      = "europe-west1"
+  name        = "fetch-reservations-scheduler-job"
+  description = "Update shop product lists"
+  schedule    = "0,20,40 0 * * *"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.function_trigger_fetch_pubsub.id
+    data       = base64encode("notused")
+  }
+}
+
+
+resource "google_pubsub_topic" "function_trigger_merge_pubsub" {
+  name    = "trigger-merge-pubsub"
+  project = var.project_id
+
+  message_storage_policy {
+    allowed_persistence_regions = [
+      "europe-west1",
+    ]
+  }
+}
+
+resource "google_cloud_scheduler_job" "merge_reservations_scheduler" {
+  project     = var.project_id
+  region      = "europe-west1"
+  name        = "merge-reservations-scheduler-job"
+  description = "Update shop product lists"
+  schedule    = "10,30,50 0 * * *"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.function_trigger_merge_pubsub.id
+    data       = base64encode("notused")
+  }
+}

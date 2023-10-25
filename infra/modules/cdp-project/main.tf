71e48ac0d17f435e59adbaa8cabc54b8b9743461
Mika Hirvikorpi <mikahirvikorpi@kiva.fi>
2023-10-25 17:01:23 +0300

    Add nelson service account

--- infra/modules/cdp-project/main.tf
+++ infra/modules/cdp-project/main.tf
@@ -11,3 +11,15 @@ resource "google_project_service" "project_cloudscheduler_api_service" {
 
   disable_dependent_services = true
 }
+
+resource "google_service_account" "bigquery_service_account" {
+  project      = var.project_id
+  account_id   = "nelson"
+  display_name = "Big Query transfer service account"
+}
+
+resource "google_project_iam_member" "cloudfunction_invoker" {
+  project = var.project_id
+  role    = "roles/cloudfunctions.invoker"
+  member  = "serviceAccount:${google_service_account.bigquery_service_account.email}"
+}

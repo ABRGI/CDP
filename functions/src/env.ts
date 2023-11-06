5bb643c7343b738c6ec1207fba46811dc9750cd2
Mika Hirvikorpi <mikahirvikorpi@kiva.fi>
2023-11-06 08:18:23 +0200

    Add Nelson API root env

--- functions/src/env.ts
+++ functions/src/env.ts
@@ -1,5 +1,6 @@
 export const googleProjectId = process.env.GCP_PROJECT_ID || "undefined"
 export const datasetId = process.env.GCP_DATASET_ID || "undefined"
+export const nelsonApiRoot = process.env.NELSON_API_ROOT || "undefined"
 
 if (googleProjectId === "undefined") {
   throw Error("GCP_PROJECT_ID environment variable not defined")
@@ -7,4 +8,8 @@ if (googleProjectId === "undefined") {
 
 if (datasetId === "undefined") {
   throw Error("GCP_DATASET_ID environment variable not defined")
+}
+
+if (nelsonApiRoot === "undefined") {
+  throw Error("NELSON_API_ROOT environment variable not defined")
 }

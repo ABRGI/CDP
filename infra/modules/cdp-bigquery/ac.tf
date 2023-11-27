3cca4d359a5f209cade961b014a10b05741a24eb
Mika Hirvikorpi <mikahirvikorpi@kiva.fi>
2023-11-27 12:40:44 +0200

    Add scheduling for ac sync

--- infra/modules/cdp-bigquery/ac.tf
+++ infra/modules/cdp-bigquery/ac.tf
@@ -33,4 +33,26 @@ resource "google_bigquery_table" "ac_contacts" {
   ])
 }
 
+resource "google_pubsub_topic" "function_trigger_ac_sync_pubsub" {
+  name    = "trigger-ac-sync-pubsub"
+  project = var.project_id
 
+  message_storage_policy {
+    allowed_persistence_regions = [
+      "europe-west1",
+    ]
+  }
+}
+
+resource "google_cloud_scheduler_job" "ac_sync_scheduler" {
+  project     = var.project_id
+  region      = "europe-west1"
+  name        = "ac-sync-scheduler-job"
+  description = "Run Active Campaign synchronization"
+  schedule    = "0 0,4,8,12,16,20 * * *"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.function_trigger_ac_sync_pubsub.id
+    data       = base64encode("notused")
+  }
+}
